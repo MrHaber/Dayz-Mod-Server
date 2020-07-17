0e481ac764a7b5c7a299123c35416cfdfb96549a
HCoder <48130257+MrHaber@users.noreply.github.com>
2020-07-18 01:55:21 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-START .\arma2oaserver.exe -mod=@hive;@dayz -name=cfgdayz -config=cfgdayz\server.cfg -cfg=cfgdayz\basic.cfg -profiles=cfgdayz -enableHT
+START .\arma2oaserver.exe -mod=@hive;@dayz -name=cfgdayz -config=cfgdayz\server.cfg -cfg=cfgdayz\basic.cfg -profiles=cfgdayz -autoinit
 EXIT
